dafcd0d4ff6ecc780bdacb55eaa0d578cb07a8f3
r-sk <073bex432.rashik@pcampus.edu.np>
2018-07-28 15:49:03 +0545

    first code added

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
-#include i dont know
+#include <opencv2/highgui.hpp>
+#include <iostream>
+ 
+int main( int argc, char** argv ) {
+  
+  cv::Mat image;
+  image = cv::imread("sample.png" , CV_HAL_DFT_STAGE_COLS);
+  
+  if(! image.data ) {
+      std::cout <<  "Could not open or find the image" << std::endl ;
+      return -1;
+    }
+  
+  cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE );
+  cv::imshow( "Display window", image );
+  
+  cv::waitKey(0);
+  return 0;
+}
